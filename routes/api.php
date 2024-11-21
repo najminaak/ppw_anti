9487f2fea03ad0608ec2f932f0f72b3af6dc7cd5
najminakinanti <najminakinantiputri@mail.ugm.ac.id>
2024-11-21 23:16:41 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
+use App\Http\Controllers\GreetController;
+use App\Http\Controllers\GalleryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/info', [InfoController::class, 'index'])->name('info');
+
+Route::get('/greet', [GreetController::class, 'greet'])->name('greet');
+
+Route::get('/gallery', [GalleryController::class, 'apiIndex'])->name('api.gallery');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
